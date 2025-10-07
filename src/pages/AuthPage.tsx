ffb0c7c2c8b95b560a4645e39c1cc9d78da9bc25
[dyad] <acproducoes21@gmail.com>
2025-10-06 21:34:27 -0300

    [dyad] Corrigindo importação do componente Link na página de autenticação - wrote 1 file(s)

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
